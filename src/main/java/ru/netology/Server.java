0bf590802941ba035de369795f5b6d2492b8a7cc
DmitryM <tihiydon@list.ru>
2023-08-13 18:34:01 +0300

    first commit

---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,108 @@
+package ru.netology;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Server {
+    private final List<String> validPaths;
+    private final ExecutorService threadPool;
+
+    public Server(List<String> validPaths, int numThreads) {
+        this.validPaths = validPaths;
+        this.threadPool = Executors.newFixedThreadPool(numThreads);
+    }
+
+    public void start(int port) {
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try {
+                    final var socket = serverSocket.accept();
+                    threadPool.submit(() -> handleConnection(socket));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleConnection(java.net.Socket socket) {
+        try (
+                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                return;
+            }
+
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                sendNotFoundResponse(out);
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            if (path.equals("/classic.html")) {
+                handleClassicHtmlRequest(out, filePath, mimeType);
+            } else {
+                handleRegularRequest(out, filePath, mimeType);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendNotFoundResponse(OutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+    private void handleClassicHtmlRequest(OutputStream out, Path filePath, String mimeType) throws IOException {
+        final var template = Files.readString(filePath);
+        final var content = template.replace(
+                "{time}",
+                LocalDateTime.now().toString()
+        ).getBytes();
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + content.length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.write(content);
+        out.flush();
+    }
+
+    private void handleRegularRequest(OutputStream out, Path filePath, String mimeType) throws IOException {
+        final var length = Files.size(filePath);
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
+    }
+}
